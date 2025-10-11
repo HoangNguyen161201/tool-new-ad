1805ef8816c0a10703a481db8910f13449603389
Nguyen quang hoang <hoangdev161201@gmail.com>
2025-10-11 10:52:55 +0700

    upgrade

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from untils import get_media_duration
+from untils import add_thumbnail_to_video
 
-result_duration = get_media_duration(f"./public/ad_videos/ad1.mkv")
-print(result_duration)
+add_thumbnail_to_video('./public/ad_videos/ad1.mkv', './public/decorates/decorate2/avatar.png', './public/decorates/decorate2/persons/person_2.png', './ttt.mkv')

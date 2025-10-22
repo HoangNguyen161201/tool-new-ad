11bf37a791d0d1c726bcf2dceaf1b05f07a60545
nguyen quang hoang <hoangdev161201@gmail.com>
2025-10-22 14:20:46 +0700

    upgrade

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-import requests
+from untils import generate_to_voice_edge
 
-proxy = "156.241.255.216:8800"
-proxies = {
-    "http": f"http://{proxy}",
-    "https": f"http://{proxy}"
-}
-
-try:
-    response = requests.get("https://api.myip.com", proxies=proxies, timeout=10)
-    if response.status_code == 200:
-        print("✅ Proxy hoạt động!")
-        print("IP qua proxy:", response.json())
-    else:
-        print("⚠️ Proxy phản hồi lỗi:", response.status_code)
-except requests.exceptions.RequestException as e:
-    print("❌ Proxy không hoạt động:", e)
+generate_to_voice_edge('nguyen quang hoàng', './t.aac')
